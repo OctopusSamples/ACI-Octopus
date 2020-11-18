a9d390da5d24b11e36e82e3943f1db80088fb971
Mike Levan <michael.levan@clouddev.engineering>
2020-11-18 12:03:29 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "location" {
+    type = string
+}
+
+variable "RG" {
+    type = string
+}
+
+variable "storageAccountName" {
+    type = string
+}
+
+variable "octopusUser" {
+    type = string
+}
+
+variable "sqlLogin" {
+    type = string
+}
+
+variable "dbpassword" {
+    type = string
+}
+
+variable "octopusPassword" {
+    type = string
+}
